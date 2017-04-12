17bf7e7845234a8a9dd94c90a41f9a96699a7373
Andy Salnikov <salnikov@slac.stanford.edu>
2017-04-12 16:53:11 -0700

    Update comments for second half of sql/RunDeepForcedSource.sql

--- sql/RunDeepForcedSource.sql
+++ sql/RunDeepForcedSource.sql
@@ -187,42 +187,119 @@ CREATE TABLE `RunDeepForcedSource` (
   `shape_sdss_flags_shift` bit(1) NOT NULL,
   `shape_sdss_flags_maxiter` bit(1) NOT NULL,
   `flux_gaussian` double DEFAULT NULL,
+        -- <descr>Uncalibrated gaussian flux.</descr>
+        -- <ucd>phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_gaussian_err` double DEFAULT NULL,
+        -- <descr>Uncertainty of flux_gaussian.</descr>
+        -- <ucd>stat.error;phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_gaussian_flags` bit(1) NOT NULL,
   `flux_gaussian_psffactor` float DEFAULT NULL,
   `flux_gaussian_flags_psffactor` bit(1) NOT NULL,
   `flux_naive` double DEFAULT NULL,
+        -- <descr>Uncalibrated naive flux.</descr>
+        -- <ucd>phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_naive_err` double DEFAULT NULL,
+        -- <descr>Uncertainty of flux_naive.</descr>
+        -- <ucd>stat.error;phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_naive_flags` bit(1) NOT NULL,
   `flux_psf` double DEFAULT NULL,
+        -- <descr>Uncalibrated PSF flux.</descr>
+        -- <ucd>phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_psf_err` double DEFAULT NULL,
+        -- <descr>Uncertainty of flux_psf.</descr>
+        -- <ucd>stat.error;phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_psf_flags` bit(1) NOT NULL,
   `flux_psf_psffactor` float DEFAULT NULL,
   `flux_psf_flags_psffactor` bit(1) NOT NULL,
   `flux_sinc` double DEFAULT NULL,
+        -- <descr>Uncalibrated sinc flux.</descr>
+        -- <ucd>phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_sinc_err` double DEFAULT NULL,
+        -- <descr>Uncertainty of flux_sinc.</descr>
+        -- <ucd>stat.error;phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `flux_sinc_flags` bit(1) NOT NULL,
   `correctfluxes_apcorr` float DEFAULT NULL,
+        -- <descr>Aperture correction factor applied to fluxes</descr>
+        -- <ucd>arith.factor</ucd>
   `correctfluxes_apcorr_flags` bit(1) NOT NULL,
   `centroid_record_x` double DEFAULT NULL,
   `centroid_record_y` double DEFAULT NULL,
   `classification_extendedness` double DEFAULT NULL,
+        -- <descr>Probability of being extended.<descr>
+        -- <ucd>stat.probability</ucd>
   `refFlux` double DEFAULT NULL,
+        -- <descr>Flux of the reference deep source on coadd.</descr>
+        -- <ucd>phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `refFlux_err` double DEFAULT NULL,
+        -- <descr>Error of lux of the reference deep source on coadd.</descr>
+        -- <ucd>phot.count;stat.uncalib</ucd>
+        -- <unit>adu</unit>
   `objectId` bigint(20) NOT NULL,
+        -- <descr>ID of object this source was assigned to. NULL if the source
+        -- did not participate in spatial clustering, or if the clustering
+        -- algorithm considered the source to be a 'noise' source.</descr>
+        -- <ucd>meta.id;src</ucd>
   `coord_raVar` float DEFAULT NULL,
+        -- <descr>Variance of ra, taken from the sample covariance matrix
+        -- of (ra, decl). The standard error of the mean ra is
+        -- sqrt(raVar/obsCount)</descr>
+        -- <ucd>stat.variance;pos.eq.ra</ucd>
+        -- <unit>arcsec^2</unit>
   `coord_radeclCov` float DEFAULT NULL,
+        -- <descr>Sample covariance of ra and decl.</descr>
+        -- <ucd>stat.covariance;pos.eq</ucd>
+        -- <unit>arcsec^2</unit>
   `coord_declVar` float DEFAULT NULL,
+        -- <descr>Variance of decl, taken from the sample covariance matrix
+        -- of (ra, decl). The standard error of the mean decl is
+        -- sqrt(declVar/obsCount)</descr>
+        -- <ucd>stat.variance;pos.eq.dec</ucd>
+        -- <unit>arcsec^2</unit>
   `exposure_id` bigint(20) NOT NULL,
+        -- <descr>Exposure identifier.</descr>
+        -- <ucd>meta.id;obs.image</ucd>
   `exposure_filter_id` int(11) NOT NULL,
+        -- <descr>Id of the filter for the band.</descr>
+        -- <ucd>meta.id;instr.filter</ucd>
   `exposure_time` float DEFAULT NULL,
+        -- <descr>Duration of exposure.</descr>
+        -- <ucd>time.duration</ucd>
+        -- <unit>s</unit>
   `exposure_time_mid` double DEFAULT NULL,
+        -- <descr>Time (ISO8601 format, UTC) at the mid-point of the
+        -- combined exposure.</descr>
+        -- <ucd>time.epoch</ucd>
   `cluster_id` bigint(20) DEFAULT NULL,
+    -- <descr> ID of a deduplication cluster.</descr>
   `cluster_coord_ra` double DEFAULT NULL,
+    -- <descr> RA-coordinate of the center of cluster.</descr>
+    -- <ucd>pos.eq.ra</ucd>
+    -- <unit>deg</unit>
   `cluster_coord_decl` double DEFAULT NULL,
+    -- <descr> Decl-coordinate of the center of cluster.</descr>
+    -- <ucd>pos.eq.dec</ucd>
+    -- <unit>deg</unit>
   `is_primary` bit(1) DEFAULT NULL,
+    -- <descr> True if does not come from de-duplication.</descr>
   `object_coord_ra` double DEFAULT NULL,
+        -- <descr>ICRS RA of object associated with this source, or ra if the
+        -- source was not associated with any object (objectId is NULL).</descr>
+        -- <ucd>pos.eq.ra</ucd>
+        -- <unit>deg</unit>
   `object_coord_decl` double DEFAULT NULL,
+        -- <descr>ICRS Dec of object associated with this source, or decl if the
+        -- source was not associated with any object (objectId is NULL).</descr>
+        -- <ucd>pos.eq.dec</ucd>
+        -- <unit>deg</unit>
   PRIMARY KEY (`id`),
   KEY `IDX_coord_htmId20` (`coord_htmId20`),
   KEY `IDX_coord_decl` (`coord_decl`),
